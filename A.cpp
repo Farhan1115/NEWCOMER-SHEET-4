b8b03e5de0a6d1b83c2d6e2702efc530959a7de1
Farhan1115 <109974258+Farhan1115@users.noreply.github.com>
2022-08-28 19:23:23 +0600

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S, T;
+    cin>>S>>T;
+    int x = S.size();
+    int y = T.size();
+    cout<<x<<" "<<y<<endl;
+    cout<<S<<" "<<T<<endl;
+    return 0;
+}
